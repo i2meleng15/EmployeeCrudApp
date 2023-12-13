5367c15c828db1e5b552ad482cf12205703ac2bd
Tumi Caleb Makatu <i2meleng15@gmail.com>
2023-12-13 07:03:42 +0000

    latest code

--- src/main/java/com/employee/EmployeeCrud/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.employee.EmployeeCrud.repository;
-
-import com.employee.EmployeeCrud.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-}
