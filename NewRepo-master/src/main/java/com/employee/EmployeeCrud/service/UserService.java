cf2d6893990910270266eb17e073dc408168f2bf
Tumi Caleb Makatu <107027745+i2meleng15@users.noreply.github.com>
2023-12-13 07:02:53 +0000

    Delete NewRepo-master directory

--- NewRepo-master/src/main/java/com/employee/EmployeeCrud/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.employee.EmployeeCrud.service;
-
-import com.employee.EmployeeCrud.dto.UserRegistrationDto;
-import com.employee.EmployeeCrud.entity.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface UserService extends UserDetailsService {
-    User save(UserRegistrationDto userRegistrationDto);
-}
